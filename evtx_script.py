242b3130f7ea1c0e958853b3934b233055423940
Fen <fen@Pinsons-MacBook-Pro.local>
2020-08-17 18:05:53 +0200

    betterparsing

--- evtx_script.py
+++ evtx_script.py
@@ -4,28 +4,38 @@ import argparse
 import xml.etree.cElementTree as ET
 import xml.dom.minidom as minidom
 import lxml.etree as etree
+import re
+import csv
 
 file_path = "./evtx.xml"
 f = open(file_path, "w")
 
 def main():
-    parse_xml(get_xml(get_args()))
+    events = parse_xml(get_xml(get_args()))
+    for event in events:
+        i = 1
+        children = event.getchildren()
+        for child in children:
+            greatchildren = child.getchildren()
+            for greatchild in greatchildren:
+                if i == 1:
+                    print("tag=%s, attrib=%s" % (re.sub( r"{.*}", "", event.tag), re.sub( r"{.*}", "", event.attrib) if event.attrib else "None"))
+                    i = 0
+                print("\t%s=%s" % (re.sub(r"{.*}", "", child.tag), child.text))
+                print("\t\t%s=%s" % (re.sub( r"{.*}", "", greatchild.tag), greatchild.text))
 
 def parse_xml(events):
     parsed_events = []
     for event in events:
         parsed_events.append(ET.fromstring(event))    #xmldoc = minidom.parse(file_path)
     return parsed_events
+
 def get_xml(args):
     with evtx.Evtx(args.evtx) as log:
-        #f.write(e_views.XML_HEADER)
-        #f.write("<Events>\n")
         events = []
         for record in log.records():
-            doc_xml = minidom.parseString(record.xml()).toprettyxml(indent="\t", newl="\n", encoding=None)
+            doc_xml = minidom.parseString(record.xml()).toprettyxml()
             events.append(doc_xml)
-            #f.write(doc_xml.replace('<?xml version="1.0" ?>\n', '').replace("<", '\t<'))
-        #f.write("</Events>")
     return events
 
 def get_args():
